a5d0b37295881a1c1097884cb924627206419c3f
Sahana Patil <sahanapatil1618@gmail.com>
2026-06-13 10:36:44 +0530

    Added image classification model deployment

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import torch
+from torchvision import models, transforms
+from torchvision.models import ResNet18_Weights
 
 app = FastAPI()
 
-class InputData(BaseModel):
-    num1: float
-    num2: float
+# Load model
+model = models.resnet18(weights=None)
+model.load_state_dict(torch.load("model.pth", map_location=torch.device("cpu")))
+model.eval()
+
+# Load category names
+weights = ResNet18_Weights.DEFAULT
+categories = weights.meta["categories"]
+
+# Image preprocessing
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
 
 @app.get("/")
 def home():
-    return {"message": "Model Deployment API is Running"}
+    return {"message": "Image Classification API is Running"}
 
 @app.post("/predict")
-def predict(data: InputData):
-    result = data.num1 + data.num2
-    return {"prediction": result}
+async def predict(file: UploadFile = File(...)):
+    
+    image = Image.open(file.file).convert("RGB")
+
+    input_tensor = transform(image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(input_batch)
+
+    predicted_class = torch.argmax(output, dim=1).item()
+    prediction = categories[predicted_class]
+
+    return {"prediction": prediction}
