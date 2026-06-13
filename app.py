3f1b3e996c5e04a3900bfaf93d26f72c78542d10
Sahana Patil <sahanapatil1618@gmail.com>
2026-06-13 10:46:12 +0530

    Fixed image classification API

--- app.py
+++ app.py
@@ -4,16 +4,24 @@ import torch
 from torchvision import models, transforms
 from torchvision.models import ResNet18_Weights
 
+
 app = FastAPI()
+
 
 # Load model
 model = models.resnet18(weights=None)
-model.load_state_dict(torch.load("model.pth", map_location=torch.device("cpu")))
+
+model.load_state_dict(
+    torch.load("model.pth", map_location=torch.device("cpu"))
+)
+
 model.eval()
 
-# Load category names
+
+# Load class names
 weights = ResNet18_Weights.DEFAULT
 categories = weights.meta["categories"]
+
 
 # Image preprocessing
 transform = transforms.Compose([
@@ -21,22 +29,33 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
+
 @app.get("/")
 def home():
-    return {"message": "Image Classification API is Running"}
+    return {
+        "message": "Image Classification API is Running"
+    }
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    
+
     image = Image.open(file.file).convert("RGB")
 
     input_tensor = transform(image)
+
     input_batch = input_tensor.unsqueeze(0)
+
 
     with torch.no_grad():
         output = model(input_batch)
 
+
     predicted_class = torch.argmax(output, dim=1).item()
+
     prediction = categories[predicted_class]
 
-    return {"prediction": prediction}
+
+    return {
+        "prediction": prediction
+    }
